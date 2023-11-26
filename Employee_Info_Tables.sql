9e086138fd8fd05729778f1462a22d61681531a5
jovanecano <jovanecano1995@gmal.com>
2023-11-25 21:06:18 -0800

    finished salaries table/last table

--- Employee_Info_Tables.sql
+++ Employee_Info_Tables.sql
@@ -4,7 +4,6 @@ CREATE TABLE titles(
 	title VARCHAR(40)
 );
 
-
 -- Creating the table for employees
 create table employees (
 	emp_no INT PRIMARY KEY NOT NULL,
@@ -26,13 +25,23 @@ CREATE TABLE departments (
 -- Creating the table for department employee
 CREATE TABLE dept_emp (
 	emp_no INT NOT NULL,
-	dept_no VARCHAR(8) NOT NULL
+	dept_no VARCHAR(8) NOT NULL,
 	FOREIGN KEY (emp_no) REFERENCES employees (emp_no),
 	FOREIGN KEY (dept_no) REFERENCES departments (dept_no)
 );
 
+--Creating the department manager table
+CREATE TABLE dept_manager (
+	dept_no VARCHAR(8) NOT NULL,
+	emp_no INT NOT NULL,
+	FOREIGN KEY (dept_no) REFERENCES departments (dept_no),
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+);
 
-
-
-
+--Creating the salaries table
+CREATE TABLE salaries (
+	emp_no INT PRIMARY KEY NOT NULL,
+	salary INT NOT NULL,
+	FOREIGN KEY (emp_no) REFERENCES employees (emp_no)
+);
 
