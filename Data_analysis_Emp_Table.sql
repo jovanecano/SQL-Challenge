9dd9ceba639a6e115f76137602c7bd7bc3c0842f
jovanecano <jovanecano1995@gmal.com>
2023-11-26 16:39:51 -0800

    data analysis cont'd

--- Data_analysis_Emp_Table.sql
+++ Data_analysis_Emp_Table.sql
@@ -5,3 +5,9 @@ SELECT sal.emp_no, em.last_name, em.first_name, em.sex, sal.salary
 FROM employees as em
 INNER JOIN salaries as sal 
 ON sal.emp_no = em.emp_no;
+
+-- List the first name, last name, and hire date for the employees who were hired in 1986
+SELECT first_name, last_name, hire_date
+FROM emmployees
+WHERE EXTRACT(YEAR FROM hire_date) = 1986;
+
